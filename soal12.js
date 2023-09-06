5487d2caf68874a4040db3489c710bf7e7e16171
mnuralim <shadownur345.com>
2023-09-06 18:51:43 +0800

    update komentar

--- soal12.js
+++ soal12.js
@@ -14,6 +14,7 @@ const checkUser = (data) => {
 
   //lakukan looping untuk cek data satu per satu
   for (let i = 0; i < data.length; i++) {
+    //jika jumlah karakter nama < 10 dan umur data lebih dari 30 ATAU gender sama dengan "male" dan eyecolor adalah brown maka push data ke array result
     if ((nameLength(data[i].name) < 10 && data[i].age > 30) || (data[i].gender === "male" && data[i].eyeColor === "brown")) {
       result.push(data[i]);
     }
--- soal17.js
+++ soal17.js
@@ -12,19 +12,26 @@
 function solution(n) {
   // tulis jawabanmu disini
   let result;
+  //jika n kurang dari sama dengan 100 dan n lebih dari sama dengan 91 maka result = "A"
   if (n <= 100 && n >= 91) {
     result = "A";
+    //jika n kurang dari sama dengan 90 dan n lebih dari sama dengan 81 maka result = "B"
   } else if (n <= 90 && n >= 81) {
     result = "B";
+    //jika n kurang dari sama dengan 80 dan n lebih dari sama dengan 71 maka result = "C"
   } else if (n <= 80 && n >= 71) {
     result = "C";
+    //jika n kurang dari sama dengan 70 dan n lebih dari sama dengan 61 maka result = "D"
   } else if (n <= 70 && n >= 61) {
     result = "D";
+    //jika n kurang dari sama dengan 60 dan n lebih dari sama dengan 0 maka result = "E"
   } else if (n <= 60 && n >= 0) {
     result = "E";
   } else if (n < 0) {
+    //jika n kurang dari 0 maka result ="nilai tidak boleh negatif"
     result = "Nilai tidak boleh negatif";
   } else {
+    //jika tidak memenuhi semua kondisi diatas maka result = "Nilai tidak boleh lebih dari 100"
     result = "Nilai tidak boleh lebih dari 100";
   }
   return result;
